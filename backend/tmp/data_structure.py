dd253a8be77523ccdff332a4679709153b8b36cf
gdroguski <g.d.roguski@gmail.com>
2023-11-23 21:06:29 +0100

    sample one step traverse update

--- backend/tmp/data_structure.py
+++ backend/tmp/data_structure.py
@@ -25,7 +25,19 @@ class Conversation:
         self.versions.pop()
 
     def __getitem__(self, version_id: str):
-        return [version for version in self.versions if version.id == version_id].pop()
+        res = [version for version in self.versions if version.id == version_id]
+        if len(res) == 0:
+            return None
+        if len(res) > 1:
+            raise Exception("Multiple versions with the same id")
+        return res.pop()
+
+    def __setitem__(self, version_id: str, version: "Version"):
+        for i, curr_version in enumerate(self.versions):
+            if curr_version.id == version_id:
+                self.versions[i] = version
+                return
+        raise Exception("No version with the given id")
 
 
 @dataclass
@@ -37,15 +49,23 @@ class Version:
     parent_version: str
     modified_at: str
     messages: list["Message"]
+    handled: bool = False
 
     def n(self):
         return len(self.messages)
 
-    def add_message(self, message):
-        self.messages.append(message)
+    def __getitem__(self, message_id: str):
+        res = [message for message in self.messages if message.id == message_id]
+        if len(res) == 0:
+            raise Exception("No message with the given id")
+        return res.pop()
 
-    def pop_message(self):
-        self.messages.pop()
+    def __setitem__(self, message_id: str, message: "Message"):
+        for i, curr_message in enumerate(self.messages):
+            if curr_message.id == message_id:
+                self.messages[i] = message
+                return
+        raise Exception("No message with the given id")
 
 
 @dataclass
@@ -104,6 +124,21 @@ def get_clean_conversation(raw_conversation):
     return conversation
 
 
+def get_branching_messages(curr_version: Version, parent_version: Version) -> tuple[Message, Message]:
+    current_messages = curr_version.messages
+    parent_messages = parent_version.messages
+
+    msg_enumerable = zip(current_messages, parent_messages)
+    n = len(current_messages)
+    for idx in range(n - 1):
+        curr_msg, parent_msg = next(msg_enumerable)
+        if curr_msg.content != parent_msg.content:
+            raise Exception("Content mismatch")
+
+    curr_branch_msg, parent_branch_msg = next(msg_enumerable)
+    return curr_branch_msg, parent_branch_msg
+
+
 def main():
     data = fetch_data()
 
@@ -111,9 +146,20 @@ def main():
     active_version_id: str = raw_conversation["active_version"]
 
     conversation = get_clean_conversation(raw_conversation)
-    curr_active_version = conversation[active_version_id]
 
-    # TBD
+    curr_active_version = conversation[active_version_id]
+    parent_version = conversation[curr_active_version.parent_version]
+
+    curr_branch_msg, parent_branch_msg = get_branching_messages(curr_active_version, parent_version)
+    curr_branch_msg.add_version(parent_version.id)
+    parent_branch_msg.add_version(curr_active_version.id)
+
+    curr_active_version[curr_branch_msg.id] = curr_branch_msg
+    curr_active_version.handled = True
+    parent_version[parent_branch_msg.id] = parent_branch_msg
+
+    conversation[active_version_id] = curr_active_version
+    conversation[parent_version.id] = parent_version
 
     a = 1
 
